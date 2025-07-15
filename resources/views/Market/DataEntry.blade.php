a9b0c49dedaddee7abbc29e08bffcb22de6d5c00
Abdullelah-Almubarak <abdullelahalmubarak@gmail.com>
2025-07-15 04:09:32 +0300

    Data Entry Page

---
+++ resources/views/Market/DataEntry.blade.php
@@ -0,0 +1,27 @@
+@extends("layout")
+@section("title","Products")
+
+@section("content")
+<div class="deMenu">
+    
+    
+   
+    <a href="/addProd">
+        <button>اضافة المنتجات</button>
+    </a>
+
+    <a href="/">
+        <button>تعديل المنتجات</button>
+    </a>
+
+    <a href="/">
+        <button>حذف المنتجات</button>
+    </a>
+    
+     <a >
+        <button>خصم المنتجات</button>
+    </a>
+
+
+</div>
+@stop
