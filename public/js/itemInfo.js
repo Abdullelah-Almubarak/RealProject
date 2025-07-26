6d0d0b1e15a4df388efb3dd18f8956e624088a58
Abdullelah-Almubarak <abdullelahalmubarak@gmail.com>
2025-07-26 13:48:24 +0300

    Fix image slider for items images

--- public/js/itemInfo.js
+++ public/js/itemInfo.js
@@ -1,24 +1,107 @@
 var slider=document.getElementsByClassName("slider")[0];
+var wrap=document.getElementsByClassName("wrapper")[0];
 var next=document.getElementsByClassName("next")[0];
 var prev=document.getElementsByClassName("prev")[0];
 var images=slider.getElementsByTagName("img");
 var imgProd=document.getElementsByClassName("info2")[0];
 var curImg=imgProd.getElementsByTagName("img")[0]
+var slideX=0;
+var drag;
 
 for(i=0;i<images.length;i++)
 {
-    images[i].addEventListener("click",function(e){
-        curImg.src=this.src;
-    })
+    images[i].data=images[i].src;
+    // alert(images[i].data);
+    images[i].addEventListener("click",function x(e){
+        if(drag==false)
+        {
+            curImg.src=this.data;
+            // console.log(drag);
+        }
+        
+
+    });
+    
 }
 
-next.addEventListener("click",function(e){
-    slider.scrollLeft+=slider.scrollWidth/5;
-});
+function nextImg(){
+    slider.scrollLeft+=slider.scrollWidth/2;
+    drag=null;
+}
 
-prev.addEventListener("click",function(e){
-    // alert(slider.scrollLeft)
-    slider.scrollLeft-=slider.scrollWidth/5;
-});
+function prevImg(){
+    slider.scrollLeft-=slider.scrollWidth/2;
+    drag=null;
+}
+
+
+
+wrap.addEventListener("touchstart",function(e){
+    slideX=e.touches[0].pageX;
+    drag=false;
+    
+})
+
+
+
+wrap.addEventListener("touchmove",function(e){
+    if(drag==false)
+    {
+        drag=true;
+    }
+    
+})
+
+
+
+document.addEventListener("touchend",function(e){
+    // console.log("fff");
+    if(e.changedTouches[0].pageX>slideX && drag==true)
+    {
+        prevImg();
+        
+    }
+    else if(e.changedTouches[0].pageX<slideX && drag==true)
+    {
+       nextImg();
+    //    console.log("fff");
+    }
+})
+
+
+
+
+wrap.addEventListener("mousedown",function(e){
+    slideX=e.pageX;
+    drag=false;
+})
+
+
+
+wrap.addEventListener("mousemove",function(e){
+    if(drag==false)
+    {
+        drag=true;
+    }
+})
+
+
+
+document.addEventListener("mouseup",function(e){
+    if(e.pageX>slideX && drag==true)
+    {
+        prevImg();
+    }
+    else if(e.pageX<slideX && drag==true)
+    {
+       nextImg();
+    }
+})
+
+
+
+next.addEventListener("click",nextImg);
+
+prev.addEventListener("click",prevImg);
 
 
